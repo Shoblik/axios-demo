ea26bf10cc639c296f18c9c2e4d2af1e32c21923
Shoblik <shoblik@yahoo.com>
2017-12-20 12:10:36 -0800

    axios call completed

--- script.js
+++ script.js
@@ -1,47 +1,9 @@
-var BASE_URL = 'http://api.reactprototypes.com';
-var API_KEY = '?key=testuser1234';
 
-axios.get(BASE_URL + '/todos' + API_KEY).then(function(resp){
-    var todos = resp.data.todos;
-    var table = $('table tbody');
+const BASE_URL = 'http://api.reactprototypes.com';
+const API_KEY = '?key=testuser1234';
 
-    // addToDom(todos, table);
-    addToDomNoMap(todos, table);
+axios.get(`${BASE_URL}/todos${API_KEY}`).then((resp) => {
+    console.log('Server response: ', resp.data);
 });
 
-var addToDom = function(list, container) {
-    console.info('Using Map');
 
-    var tableRows = list.map((item, index) => {
-
-        var tableData = [
-            $(`<td>${index + 1}</td>`),
-            $(`<td>${item.title}</td>`),
-            item.complete ? $(`<td class="text-success">Yes</td>`) : $(`<td class="text-danger">No</td>`)
-        ];
-
-        return $('<tr>').append(tableData);
-    });
-
-    container.append(tableRows);
-};
-
-var addToDomNoMap = function(list, container) {
-    console.info('Using For Loop');
-
-    var tableRows = [];
-
-    for(var i = 0; i < list.length; i++){
-        var item = list[i];
-
-        var tableData = [
-            $(`<td>${i + 1}</td>`),
-            $(`<td>${item.title}</td>`),
-            item.complete ? $(`<td class="text-success">Yes</td>`) : $(`<td class="text-danger">No</td>`)
-        ];
-
-        tableRows.push($('<tr>').append(tableData));
-    }
-
-    container.append(tableRows);
-};
